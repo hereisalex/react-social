3601ee85415f177e0dba98db09db5b959ce70efd
Alex McKenzie <here.is.alex@gmail.com>
2021-11-26 00:47:39 -0600

    add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React from "react";
 import Home from "./pages/home/Home";
+import Login from "./components/login/Login";
 import Profile from "./components/profile/Profile";
+import Register from "./components/register/Register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 function App() {
-  return <Profile />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/register">
+          <Register />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/register/Register.jsx
@@ -0,0 +1,24 @@
+import "./register.css";
+
+export default function Register() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">basic</h3>
+          <span className="loginDesc">A place to connect--without the BS.</span>
+        </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <input placeholder="Username" className="loginInput" />
+            <input placeholder="Email" className="loginInput" />
+            <input placeholder="Password" className="loginInput" />
+            <input placeholder="Password Again" className="loginInput" />
+            <button className="loginButton">Sign Up</button>
+            <button className="loginRegisterButton">Login </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
